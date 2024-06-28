44e45d769bdb025ab4dbe76f547d7c09d5459640
Rakibur Rahaman <mail@rakibpro.com>
2024-06-29 03:32:42 +0600

    fasdfdf

---
+++ components/DemoLamp.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { LampContainer } from "./ui/lamp";
+
+export function LampDemo() {
+  return (
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Donwload videos
+        <br />
+        in the right way
+      </motion.h1>
+
+      <div className="flex flex-col items-center justify-center">
+        <input
+          type="text"
+          placeholder="Paste the video link here"
+          className="w-[500px] mt-20 h-12 p-4 text-lg bg-black border-2 border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-transparent"
+        />
+        <button className="relative mt-10 inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Lets Dive ⚡
+          </span>
+        </button>
+      </div>
+    </LampContainer>
+  );
+}
